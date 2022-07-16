9fe5bf4ac251982b1b7d4d394fe14285ff137eb0
naorigg <shikugen.chi@gmail.com>
2022-07-16 20:11:27 +0100

    Fix Bug Command

--- src/main/java/jp/naori/naoMachiPlugin/commands/Hello.java
+++ src/main/java/jp/naori/naoMachiPlugin/commands/Hello.java
@@ -2,11 +2,11 @@ package jp.naori.naoMachiPlugin.commands;
 
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class Hello extends JavaPlugin {
+public class Hello implements CommandExecutor {
 
     public Hello(){
 
